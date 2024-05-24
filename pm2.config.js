2e9ac5fa45abedddac40b6e13ae825266fa05253
cubesoftech <einsoftdev@gmail.com>
2024-05-24 18:02:55 +0800

    Update pm2.config.js to remove autorestart and watch options

--- pm2.config.js
+++ pm2.config.js
@@ -6,8 +6,6 @@ module.exports = {
       script: "npm",
       args: "start",
       instances: 1,
-      autorestart: true,
-      watch: false,
       max_memory_restart: "1G",
       env: {
         NODE_ENV: "production",
