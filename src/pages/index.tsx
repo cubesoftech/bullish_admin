68180b371a7f01af714a167e980f932cca1d73df
cubesoftech <einsoftdev@gmail.com>
2024-05-29 20:21:41 +0800

    chore: Add lazy loading for images in Home component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,8 @@ import { Bounce, toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { SocketListenerPayload } from "@/utils/interface";
 import SoundPlayer from "./components/utils/SoundPlayer";
+import useSWR from "swr";
+import axios from "axios";
 // import useSound from "use-sound";
 
 export default function Home() {
@@ -24,8 +26,43 @@ export default function Home() {
   //   interrupt: true,
   // });
 
+  useSWR(
+    "https://mslot10.com/reports",
+    async (url) => {
+      const res = await axios.get<SocketListenerPayload>(url);
+      return res.data;
+    },
+    {
+      refreshInterval: 1000,
+      onSuccess: (data) => {
+        const { withdrawals, deposits, inquires, newmembers, trades } = data;
+        console.log({ withdrawals, deposits, inquires, newmembers, trades });
+        if (withdrawals) {
+          handlePlaySound();
+          toast.info(`New Withdrawal Request: ${withdrawals} withdrawals`);
+        }
+        if (deposits) {
+          handlePlaySound();
+          toast.info(`New Deposit Request: ${deposits} deposits`);
+        }
+        if (inquires) {
+          handlePlaySound();
+          toast.info(`New Inquiry: ${inquires} inquries`);
+        }
+        if (newmembers) {
+          handlePlaySound();
+          toast.info(`New Member: ${newmembers} new members`);
+        }
+        if (trades) {
+          handlePlaySound();
+          toast.info(`New Trade: ${trades} trades`);
+        }
+      },
+    }
+  );
+
   useEffect(() => {
-    const socket_server = "https://mslot10.com/";
+    const socket_server = "http://localhost:9000";
     const socket = io(socket_server);
 
     //listen to event observerChanges
