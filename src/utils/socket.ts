c9ea3f5bc2ad20afcd740413e065dbe218576f5b
Aries <aries.dimailig8@gmail.com>
2025-04-11 10:37:31 +0800

    update socket server

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-export const socket_server = "https://server.sjinvestment.net/";
+export const socket_server = "https://server.sj-investment.net/";
 
 export const socket = io(socket_server)
